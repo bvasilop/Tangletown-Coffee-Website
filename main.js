84df3e952b4610116442c03007b6469f39e28fbf
bvasilop <bvasilop@gmail.com>
2017-05-20 12:32:12 -0700

    Added footer

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
-/*$(document).ready(function(){
 
-function myMap() {
-var myCenter = new google.maps.LatLng(47.668673, -122.333144);
-var mapProp = {center:myCenter, zoom:18, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-var marker = new google.maps.Marker({position:myCenter});
-marker.setMap(map);$('[data-toggle="tooltip"]').tooltip();
-});
-// Add smooth scrolling to all links in navbar + footer link
-$(".navbar a, footer a[href='billvas.com']").on('click', function(event) {
+$(document).on('ready', function(){
+    // Form validation
+    $('#name').validate({
+        submitHandler: function(form) {
+            // If form is valid, submit it!
+            form.submit();
+        },
+        rules: {
+            "your-name": {
+                required: true,
+                maxlength: 128,
+                digits: false
+            }
 
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 900, function(){
-
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
+        }
     });
-  } // End if
-});
+    $('#email').validate({
+        submitHandler: function(form) {
+            // If form is valid, submit it!
+            form.submit();
+        },
+        rules: {
+            "your-email": {
+                required: true,
+                maxlength: 128,
+                digits: false
+              }
 
+          }
+      });
+    // Tooltips
+    $('label span.glyphicon').tooltip();
 });
-*/
