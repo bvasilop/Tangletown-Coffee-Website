4428e379c602bc0b41c9df1a75be2f10fd81c130
bvasilop <bvasilop@gmail.com>
2017-05-20 20:51:07 -0700

    Added 1.75vw for p

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ $(document).on('ready', function(){
 
         }
     });
-    $('#email').validate({
+    $('#name').validate({
         submitHandler: function(form) {
             // If form is valid, submit it!
             form.submit();
@@ -25,25 +25,7 @@ $(document).on('ready', function(){
                 required: true,
                 maxlength: 128,
                 digits: false
-              }
-
-          }
-      });
+                
     // Tooltips
     $('label span.glyphicon').tooltip();
-  /*  // Modal popup section
-       $('#demo').on('show.bs.modal', function (event) {
-           var button = $(event.relatedTarget); // Button that triggered the modal
-           var title = button.data('title'); // Extract info from data-* attributes
-           var imgSrc = button.data('imgsrc');
-           var imageDescription = button.data('description');
-
-           // Update the modal's content. We'll use jQuery here.
-           var modal = $(this);
-           modal.find('.modal-title').html(title);
-           var modalBody = modal.find('.modal-body');
-           modalBody.empty();
-           var modalDescription = $("<p class='image-description'>").html(imageDescription).appendTo(modalBody);
-       });*/
-
 });
