eadec92c357ecbe00b5625be9a870ec16f67072b
bvasilop <bvasilop@gmail.com>
2017-05-22 17:23:02 -0700

    cleaned up main.js

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+  // Added smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Used jQuery's animate() method to add smooth page scroll
+      // Used (900) milliseconds for how long it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+})
 
-$(document).on('ready', function(){
+function myMap() {
+var myCenter = new google.maps.LatLng(47.668673, -122.333144);
+var mapProp = {center:myCenter, zoom:15, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var marker = new google.maps.Marker({position:myCenter});
+marker.setMap(map);
+}
+
+
+
+
+
+
+
+
+
+/*$(document).on('ready', function(){
     // Form validation
     $('#name').validate({
         submitHandler: function(form) {
@@ -25,7 +63,8 @@ $(document).on('ready', function(){
                 required: true,
                 maxlength: 128,
                 digits: false
-                
+
     // Tooltips
     $('label span.glyphicon').tooltip();
 });
+*/
