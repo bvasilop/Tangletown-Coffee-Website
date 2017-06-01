2b9bb1532ac1441025dca820ffe6c303b52ff9df
bvasilop <bvasilop@gmail.com>
2017-06-01 12:39:33 -0700

    Added credit card jquery plugin

--- js/shop.js
+++ js/shop.js
@@ -1,94 +1,6 @@
 /* Coffee Purchase-- FORM VALIDATION */
-
-/*$(document).on("ready", function(){
-    // Form validation
-    // alphanumeric validator
-  $.validator.addMethod("alphanumeric", function(value, element) {
-       return this.optional(element) || /^[A-Za-z][A-Za-z0-9]*$/i.test(value);
-  }, "Letters, numbers, and underscores only please" );
-  // define object to validate form data
-    $("#order-form").validate({
-      // Connect the validation object to an event handler tied to the submit button.
-        submitHandler: function(form) {
-            // If valid, then submit
-            form.submit();
-        },
-        // validate form data with the following rules
-        rules:
-            // name
-            {
-              "your-name": {
-                  required: true,
-                  maxlength: 128,
-              },
-              //address
-              "your-address": {
-                required: true
-              },
-              // additional address no more than 500 characters
-              "your-address2": {
-                required: false,
-              },
-              // city
-              "your-city":{
-                required: true
-              },
-              // state no more than two letters
-              "your-state": {
-                  required: true,
-                  minlength: 2,
-                  maxlength: 2,
-
-              },
-              // zip code no more or less  than 5 digits
-              "your-zip": {
-                  required: true,
-                  maxlength: 5,
-                  minlength:5,
-                  integer: true,
-                  digits: true
-              },
-              // cardholder name no more than 128 characters
-              "card-holder-name": {
-                  required: true,
-                  maxlength: 128
-              },
-              // card number
-              "card-number": {
-                  required: true,
-                  creditcard: true
-              },
-              // expiration month
-              "expiry-month": {
-                  required: true
-              },
-
-              // expiration year
-              "expiry-year": {
-                  required: true
-              },
-
-            // three digit code no more or less than 3 digits
-              "cvv": {
-                  required: true,
-                  minlength: 3,
-                  maxlength: 3,
-                  integer: true,
-                  digits: true
-              },
-               // optional additional instructions
-              "comments": {
-                required: false,
-                maxlength: 500
-              }
-           }
-    });
-    // Tooltips
-    $('label span.glyphicon').tooltip();
-});*/
-
-
 $(document).on('ready', function(){
+  // alphanumeric validator
   $.validator.addMethod( "integer", function( value, element ) {
    return this.optional( element ) || /^-?\d+$/.test( value );
    }, "Please enter a valid 5-digit US zip code" );
@@ -119,7 +31,7 @@ $(document).on('ready', function(){
             "card-number": {
                 required: true,
                 creditcard: true,
-                
+
             },
             "cvv": {
                 required: true,
