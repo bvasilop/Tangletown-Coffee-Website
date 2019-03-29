d4beeb1ab322b1ead41a84a6acd70d05eaa1358b
bvasilop <bvasilop@gmail.com>
2019-03-28 18:07:36 -0700

    used beautify formatting to clean up code

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
-  // Initialize Tooltip
-  //carousel for images
-  $("#myCarousel").carousel({
-    interval: 5500
-  });
-  $('[data-toggle="tooltip"]').tooltip();
-  // Added smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-      // Store hash
-      var hash = this.hash;
-      // Used jQuery's animate() method to add smooth page scroll
-      // Used (900) milliseconds for how long it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1000, function(){
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-})
+$(document)
+    .ready(function () {
+        // Initialize Tooltip carousel for images
+        $("#myCarousel").carousel({interval: 5500});
+        $('[data-toggle="tooltip"]').tooltip();
+        // Added smooth scrolling to all links in navbar + footer link
+        $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+                // Store hash
+                var hash = this.hash;
+                // Used jQuery's animate() method to add smooth page scroll Used (900)
+                // milliseconds for how long it takes to scroll to the specified area
+                $('html, body').animate({
+                    scrollTop: $(hash)
+                        .offset()
+                        .top
+                }, 1000, function () {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
+            } // End if
+        });
+    })
